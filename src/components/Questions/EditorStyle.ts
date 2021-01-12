93a5c3acaa5380375f82b20234d57626643d057e
DESKTOP-3P4ESAE\Kuba <kuba9519@gmail.com>
2021-01-12 08:42:00 +0100

    Add pagination.

--- src/components/Questions/EditorStyle.ts
+++ src/components/Questions/EditorStyle.ts
@@ -1,5 +1,12 @@
 import styled from "styled-components";
-import { contentDisplay } from "../../styles/values";
+import {
+  contentDisplay,
+  buttonColor,
+  shadow,
+  borderRadius,
+  darkButtonColor,
+  logoColor,
+} from "../../styles/values";
 
 const StyledDiv = styled.div`
   ${contentDisplay};
@@ -16,6 +23,50 @@ const StyledDiv = styled.div`
 
   > .questionLink ~ .questionLink {
     margin-top: 1.5rem;
+  }
+
+  .paginationContainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    li {
+      margin-bottom: 1rem;
+      margin-right: 1rem;
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      &.selected a {
+        background: ${logoColor};
+      }
+
+      &.paginationMove a {
+        display: flex;
+        height: 3.4rem;
+
+        & i {
+          cursor: pointer;
+          
+        }
+      }
+
+      a {
+        border: none;
+        background: ${buttonColor};
+        box-shadow: ${shadow};
+        padding: 5px 10px;
+        border-radius: ${borderRadius};
+        position: relative;
+        cursor: pointer;
+
+        &:hover {
+          background: ${darkButtonColor};
+        }
+      }
+    }
   }
 `;
 
