1600c917ffc03939be2a5068d98a525560644937
nanoblit <jakub.k.maleta@gmail.com>
2024-07-13 20:50:18 +0200

    Switch to hash router

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,7 +1,7 @@
 import "./wdyr";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
@@ -10,9 +10,9 @@ import { store } from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
